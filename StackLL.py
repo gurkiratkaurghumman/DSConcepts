a0b2db453a4869f7d8b8bd6377b2aa1effd6f35b
gurkiratkaurghumman <71826973+gurkiratkaurghumman@users.noreply.github.com>
2022-04-11 23:40:05 +0530

    Add files via upload

---
+++ StackLL.py
@@ -0,0 +1,52 @@
+# STACK USING lINKED LIST
+
+
+
+class Stack:
+    def __init__(self,maxsize):
+        self.maxsize=maxsize
+        self.elements=[None]*maxsize
+        self.top=-1
+
+    def isEmpty(self):
+        if self.top==-1:
+            return True
+        return False
+
+    def isFull(self):
+        if self.top==self.maxsize-1:
+            return True
+        return False
+
+    def push(self,data):
+        if (self.isFull()):
+            print("stack full")
+        else:
+            self.top=self.top+1
+            self.elements[self.top]=data
+
+    def pop(self):
+        if self.top==-1:
+            print("stack empty")
+        else:
+            data=self.elements[self.top]
+            self.top=self.top-1
+            return data
+    
+    def display(self):
+        if(self.isEmpty()):
+            print("empty")
+        index=self.top
+        print("top to down")
+        while(index>=0):
+            print(self.elements[index])
+            index-=1
+        
+stack=Stack(10)
+stack.push(1)
+stack.push(2)
+stack.push(4)
+stack.push(3)
+stack.pop()
+stack.push(5)
+stack.display()
